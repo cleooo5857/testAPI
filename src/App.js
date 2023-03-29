6fd2a441619821ed88abe6e9f9c19810c59092fe
cleooo5857 <zleooo@naver.com>
2023-03-29 16:25:59 +0900

    testAPI 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [email,setemail] = useState();
+  const [psw,setpsw] = useState();
+
+  const emailtest = (e) => {
+    setemail(e.target.value);
+  }
+
+  const pswtest = (e) => {
+    setpsw(e.target.value);
+  }
+  
+
+  const testAPI= () => {
+  
+    fetch("http://localhost:3001/posts/", {
+    method : "POST",
+    body : JSON.stringify(email,psw),
+    headers: {"Content-Type": "application/json"},
+    }).then(response => response.json()).then((json) => console.log(json))
+  }
+
+  console.log(email);
+  console.log(psw);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input style={{display:"block"}} type="text" email ={email}  onChange={emailtest} placeholder="아이디"/>
+        <input style={{display:"block"}} type="password"  psw ={psw} onChange={pswtest} placeholder="비밀번호"/>
+      </div>
+      <button onClick={testAPI}>회원가입</button>
     </div>
   );
 }
